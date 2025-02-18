9adc7c14fc12e5652bfe04b7c0fa3272bc6e301d
A.S.M Imrul Kayes <83666751+asmimrul007@users.noreply.github.com>
2025-02-18 22:25:10 +0600

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,11 @@
-
+/*
 export const getImageUrl =(path) => {
     return new URL(`public/images/${path}`, window.location.origin).href;
-} ;
+} ; 
+*/
 
+
+export const getImageUrl =(path) => {
+    return `public/images/${path}`;
+} ;
 
